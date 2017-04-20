d87055dc8447e6c6e52404977d5d2893c2b2b360
Katie Patrick <kp@zerowaste.ucsc.edu>
2017-04-20 01:41:09 +0000

    Add CORS support

--- serve.js
+++ serve.js
@@ -7,6 +7,12 @@ var config = require('./config'); // get our config file
 // Api Router Handler
 var api = express.Router();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 api.get('/db', function(req, res){
 
     var sql = require("mssql");
