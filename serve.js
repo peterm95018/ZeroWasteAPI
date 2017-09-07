7fa54ca1e4266243245df7dcd4e4f06ff625c454
DeliSauce <nycdelisauce@gmail.com>
2017-09-06 19:45:54 -0700

    Add comments

--- serve.js
+++ serve.js
@@ -5,8 +5,14 @@ var bodyParser  = require('body-parser');
 // Api Router Handler
 var api = express.Router();
 
+//2017 Spring Quarter data (approx 60 days)
 const sqlQuery1 = `SELECT * FROM (SELECT *, ROW_NUMBER() OVER (ORDER BY Record DESC) as row FROM ExportLoadData) a WHERE PickupTime BETWEEN '2017-04-04' AND '2017-06-04'`;
-const sqlQuery2 = 'SELECT * FROM (SELECT *, ROW_NUMBER() OVER (ORDER BY Record DESC) as row FROM ExportLoadData) a WHERE PickupTime >= DATEADD(day,-10, GETDATE())';
+//last 60 days
+const sqlQuery2 = 'SELECT * FROM (SELECT *, ROW_NUMBER() OVER (ORDER BY Record DESC) as row FROM ExportLoadData) a WHERE PickupTime >= DATEADD(day,-60, GETDATE())';
+
+
+
+
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -27,23 +33,23 @@ api.get('/db', function(req, res){
     pool1.request() // or: new sql.Request(pool1)
     .query(sqlQuery1, (err, result) => {
         res.json(result);
-      })
-  })
+      });
+  });
 
   pool1.on('error', err => {
      res.json(result);
-  })
+  });
 
   const pool2 = new sql.ConnectionPool(sql.config, err => {
       pool2.request() // or: new sql.Request(pool2)
       .query(sqlQuery1, (err, result) => {
     // ... error checks
-      })
-  })
+    });
+  });
 
   pool2.on('error', err => {
       // ... error handler
-  })
+  });
 });
 
 app.get('/', function(req, res){
