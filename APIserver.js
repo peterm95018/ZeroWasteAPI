1026fc0005dc0ba52fcc0d2b5474794a36ade906
DeliSauce <nycdelisauce@gmail.com>
2017-09-11 21:53:35 -0700

    Update queries.

--- APIserver.js
+++ APIserver.js
@@ -6,7 +6,6 @@ var bodyParser  = require('body-parser');
 var api = express.Router();
 
 const db = 'WebLoadDataSource';
-// const db2 = 'ExportLoadData';
 
 //2017 Spring Quarter dates (approx 60 days)
 //https://registrar.ucsc.edu/calendar/key-dates/index.html
@@ -15,15 +14,14 @@ const springEnd = `'2017-06-15'`;
 
 //2017 Fall Quarter dates
 //https://registrar.ucsc.edu/calendar/archive/2016-17calendar-two-pages.pdf
-const fallStart = `'2017-08-23'`;
+const fallStart = `'2017-09-23'`;
 const fallEnd = `'2017-12-15'`;
 
-const querySpring = `SELECT * FROM ${db} WHERE PickupTime BETWEEN ${springStart} AND ${springEnd} ORDER BY PickupTime ASC`;
-const queryFall = `SELECT * FROM ${db} WHERE PickupTime BETWEEN ${fallStart} AND ${fallEnd} ORDER BY PickupTime ASC`;
+const querySpring = `SELECT * FROM ${db} WHERE PickupTime BETWEEN ${springStart} AND DATEADD(day, 1, ${springEnd}) ORDER BY PickupTime ASC`;
+const queryFall = `SELECT * FROM ${db} WHERE PickupTime BETWEEN ${fallStart} AND DATEADD(day, 1, ${fallEnd}) ORDER BY PickupTime ASC`;
 const query30days = `SELECT * FROM ${db} WHERE PickupTime >= DATEADD(day,-30, GETDATE()) ORDER BY PickupTime ASC`;
 const query45days = `SELECT * FROM ${db} WHERE PickupTime >= DATEADD(day,-45, GETDATE()) ORDER BY PickupTime ASC`;
 const query60days = `SELECT * FROM ${db} WHERE PickupTime >= DATEADD(day,-60, GETDATE()) ORDER BY PickupTime ASC`;
-// const queryFall = `SELECT * FROM ${db} WHERE PickupTime >= ${fallStart} ORDER BY PickupTime ASC`;
 // const sqlQuery = `SELECT * FROM (SELECT *, ROW_NUMBER() OVER (ORDER BY Record DESC) as row FROM ${db1}) a WHERE PickupTime >= DATEADD(day,-30, GETDATE())`;
 
 app.use(function(req, res, next) {
@@ -58,6 +56,9 @@ const createPools = function(query) {
 };
 
 api.get('/db', createPools(query60days));
+api.get('/30', createPools(query30days));
+api.get('/45', createPools(query45days));
+api.get('/60', createPools(query60days));
 api.get('/2017/fall', createPools(queryFall));
 api.get('/2017/spring', createPools(querySpring));
 
